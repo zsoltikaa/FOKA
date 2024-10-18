423b725ddcd474462622e50bffb9bbe5780f48aa
zsoltikaa <padarzsolti@gmail.com>
2024-10-18 23:23:31 +0200

    made the hamburger button work correctly

--- js/script.js
+++ js/script.js
@@ -2,6 +2,24 @@
 
 function myFunction(x) {
     x.classList.toggle("change");
+}
+
+let disabled = true;
+let pageSelector = document.querySelector(".page-selector");
+
+document.querySelector(".container").addEventListener("click", () => {
+
+  if (disabled) 
+  {
+    pageSelector.classList.remove("inactive");
   }
+  else 
+  {
+    pageSelector.classList.add("inactive");
+  }
+
+  disabled = !disabled;
+
+});
 
 // HAMBURGER BUTTON TOGGLE END
